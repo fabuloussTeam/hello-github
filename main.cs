0f2f84cebf06ed9d55833f7803dc54e574ad0ffe
tjomemmanuel <tjomemmanuel@gmail.com>
2024-04-01 11:05:11 -0400

    Creation de mon projet et de la page principale main

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+
+namespace Module11git;
+
+public class Main
+{
+     public static void Main()
+    {
+        Console.WriteLine("Hello, GitHub! Je m'appelle EMMANUEL TJOM");
+    }
+}
